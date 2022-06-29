9aaf065b86f0ba7d9501575ae24f5bc671232ce7
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-06-29 10:23:12 +0100

    addded some script for taking in values

---
+++ script.js
@@ -0,0 +1,8 @@
+function getMarks(){
+
+    var maths = document.getElementById("maths").value;
+    var physics = document.getElementById("physics").value;
+    var chemistry = document.getElementById("chemistry").value;
+    var biology = document.getElementById("biology").value;
+    console.log(maths + physics + chemistry + biology);
+}
