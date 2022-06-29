302b26139b617dfc613d6914e576fc1131f234a5
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-06-29 10:53:03 +0100

    my final commit on student_grade_calculator

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 function getMarks(){
 
-    var maths = document.getElementById("maths").value;
-    var physics = document.getElementById("physics").value;
-    var chemistry = document.getElementById("chemistry").value;
-    var biology = document.getElementById("biology").value;
-    console.log(maths + physics + chemistry + biology);
+    var maths = Number(document.getElementById("maths").value);
+    var physics = Number(document.getElementById("physics").value);
+    var chemistry = Number(document.getElementById("chemistry").value);
+    var biology = Number(document.getElementById("biology").value);
+    var totalMarks = 400;
+    var marksObtained = maths + physics + chemistry + biology;
+    var percentage = (marksObtained / totalMarks) * 100;
+     var grade ="";
+document.getElementById("percentage").innerHTML = percentage + "%";
+
+if(percentage > 90) {
+    grade = "A+";
+} else if(percentage < 90 && percentage > 80) {
+    grade ="A";
+ } else if (percentage < 80 && percentage> 60) {
+    grade = "B";
+ } else if (percentage < 60 ) {
+    grade = "F";
+ }
+
+document.getElementById("grade").innerHTML = grade;
+if(percentage < 60) {
+    document.getElementById("pass").innerHTML = "Sorry you failed! please try next time.";
+} else {
+    document.getElementById("pass").innerHTML = "Congratulation!! You have been passed!";
+}  
+
+     
 }
